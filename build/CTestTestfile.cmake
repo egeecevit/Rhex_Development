6ec6023e7208964f6d7afd70f6bb9aff5833ee1b
egeecevit <egeecevit@gmail.com>
2024-06-03 13:11:05 +0000

    rebuild with colcon

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dev-ros2/development/src/rhex_description
-# Build directory: /home/dev-ros2/development/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tbt_otonom_sim/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/dev-ros2/development/install/control_msgs/share/control_msgs/package.sh"
